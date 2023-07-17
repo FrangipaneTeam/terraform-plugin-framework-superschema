ba8cbc1fde5f39dcda6c822c37214fc6c68cb13a
Mickael Stanislas <contact@mickael-stanislas.com>
2023-07-17 16:51:16 +0200

    feat: add supertypes

--- template.go
+++ template.go
@@ -21,6 +21,9 @@ type templateInfos struct {
 //go:embed type_attribute.go.tmpl
 var templateTypeAttribute string
 
+//go:embed supertype_attribute.go.tmpl
+var templateSuperTypeAttribute string
+
 func main() {
 	fmt.Println("generating types files...")
 	tA := []string{"string", "bool", "float64", "int64", "list", "list_nested", "object", "single_nested", "set", "set_nested", "number", "map", "map_nested"}
@@ -30,31 +33,52 @@ func main() {
 			TypeName: strcase.ToCamel(t),
 		}
 
-		tmpl, err := template.New("template").Parse(templateTypeAttribute)
+		tmplType, err := template.New("template").Parse(templateTypeAttribute)
 		if err != nil {
 			fmt.Printf("error from template parse : %v\n", err)
 			os.Exit(1)
 		}
 
-		var tpl bytes.Buffer
+		tmplSuperType, err := template.New("template").Parse(templateSuperTypeAttribute)
+		if err != nil {
+			fmt.Printf("error from template parse : %v\n", err)
+			os.Exit(1)
+		}
 
-		errExec := tmpl.Execute(&tpl, infos)
+		var (
+			tpl      bytes.Buffer
+			tplSuper bytes.Buffer
+		)
 
-		if errExec != nil {
-			fmt.Printf("error from template execute : %v\n", errExec)
+		if err := tmplType.Execute(&tpl, infos); err != nil {
+			fmt.Printf("error from template execute : %v\n", err)
+			os.Exit(1)
+		}
+
+		if err := tmplSuperType.Execute(&tplSuper, infos); err != nil {
+			fmt.Printf("error from template execute : %v\n", err)
 			os.Exit(1)
 		}
 
 		// format the code
-		formattedContent, errFormat := format.Source(tpl.Bytes())
+		formattedTmplType, errFormat := format.Source(tpl.Bytes())
 		if errFormat != nil {
 			fmt.Printf("error from format : %v\n", errFormat)
 			os.Exit(1)
 		}
 
-		errWrite := os.WriteFile(t+"_attribute.go", formattedContent, 0o644)
-		if errWrite != nil {
-			fmt.Printf("write to file error : %v\n", errWrite)
+		formattedTmplSuperType, errFormat := format.Source(tplSuper.Bytes())
+		if errFormat != nil {
+			fmt.Printf("error from format : %v\n", errFormat)
+			os.Exit(1)
+		}
+
+		if err := os.WriteFile(t+"_attribute.go", formattedTmplType, 0644); err != nil {
+			fmt.Printf("write to file error : %v\n", err)
+		}
+
+		if err := os.WriteFile("super"+t+"_attribute.go", formattedTmplSuperType, 0644); err != nil {
+			fmt.Printf("write to file error : %v\n", err)
 		}
 	}
 }
