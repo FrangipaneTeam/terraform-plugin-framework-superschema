849056a1dd90559065cf3a53750f14a5113b0549
Mickael Stanislas <contact@mickael-stanislas.com>
2024-01-17 15:52:09 +0100

    feat: add set/list/mapAttributeOf (#66)

--- superlist_attribute.go
+++ superlist_attribute.go
@@ -167,3 +167,148 @@ func (s SuperListAttribute) GetDataSource(ctx context.Context) schemaD.Attribute
 	a.MarkdownDescription = genDataSourceAttrDescription(ctx, a.MarkdownDescription, deprecationMessage, a.Validators)
 	return a
 }
+
+// * SuperTypeOf
+
+var _ Attribute = SuperListAttributeOf[struct{}]{}
+
+type SuperListAttributeOf[T any] struct {
+	Deprecated *Deprecated
+	Common     *schemaR.ListAttribute
+	Resource   *schemaR.ListAttribute
+	DataSource *schemaD.ListAttribute
+}
+
+// IsResource returns true if the attribute is a resource attribute.
+func (s SuperListAttributeOf[T]) IsResource() bool {
+	return s.Resource != nil || s.Common != nil
+}
+
+// IsDataSource returns true if the attribute is a data source attribute.
+func (s SuperListAttributeOf[T]) IsDataSource() bool {
+	return s.DataSource != nil || s.Common != nil
+}
+
+//nolint:dupl
+func (s SuperListAttributeOf[T]) GetResource(ctx context.Context) schemaR.Attribute {
+	var (
+		common   schemaR.ListAttribute
+		resource schemaR.ListAttribute
+	)
+
+	if s.Common != nil {
+		common = *s.Common
+	}
+
+	if s.Resource != nil {
+		resource = *s.Resource
+	}
+
+	a := schemaR.ListAttribute{
+		Required:            computeIsRequired(common, resource),
+		Optional:            computeIsOptional(common, resource),
+		Computed:            computeIsComputed(common, resource),
+		Sensitive:           computeIsSensitive(common, resource),
+		MarkdownDescription: computeMarkdownDescription(common, resource),
+		Description:         computeDescription(common, resource),
+		DeprecationMessage:  computeDeprecationMessage(common, resource),
+		ElementType:         common.ElementType,
+	}
+
+	a.Validators = append(a.Validators, common.Validators...)
+	a.Validators = append(a.Validators, resource.Validators...)
+	a.PlanModifiers = append(a.PlanModifiers, common.PlanModifiers...)
+	a.PlanModifiers = append(a.PlanModifiers, resource.PlanModifiers...)
+
+	defaultVDescription := ""
+
+	if s.Common != nil {
+		if s.Common.CustomType != nil {
+			a.CustomType = s.Common.CustomType
+		}
+	}
+
+	if s.Resource != nil {
+		if s.Resource.Default != nil {
+			a.Default = s.Resource.Default
+			defaultVDescription = s.Resource.Default.MarkdownDescription(ctx)
+		}
+		if s.Resource.CustomType != nil {
+			a.CustomType = s.Resource.CustomType
+		}
+	}
+	// * If user has not provided a custom type, we will use the default supertypes
+	if a.CustomType == nil {
+		a.CustomType = supertypes.NewListTypeOf[T](ctx)
+	}
+
+	deprecationMessage := ""
+	if s.Deprecated != nil {
+		a.DeprecationMessage = s.Deprecated.DeprecationMessage
+		deprecationMessage = s.Deprecated.computeDeprecatedDocumentation()
+	}
+
+	a.MarkdownDescription = genResourceAttrDescription(ctx, a.MarkdownDescription, defaultVDescription, deprecationMessage, a.Validators, a.PlanModifiers)
+	return a
+}
+
+//nolint:dupl
+func (s SuperListAttributeOf[T]) GetDataSource(ctx context.Context) schemaD.Attribute {
+	var (
+		common     schemaR.ListAttribute
+		dataSource schemaD.ListAttribute
+	)
+
+	if s.Common != nil {
+		common = *s.Common
+	}
+
+	if s.DataSource != nil {
+		dataSource = *s.DataSource
+	}
+
+	a := schemaD.ListAttribute{
+		Required:            computeIsRequired(common, dataSource),
+		Optional:            computeIsOptional(common, dataSource),
+		Computed:            computeIsComputed(common, dataSource),
+		Sensitive:           computeIsSensitive(common, dataSource),
+		MarkdownDescription: computeMarkdownDescription(common, dataSource),
+		Description:         computeDescription(common, dataSource),
+		DeprecationMessage:  computeDeprecationMessage(common, dataSource),
+		ElementType:         common.ElementType,
+	}
+
+	if s.DataSource != nil {
+		if s.DataSource.ElementType != nil {
+			a.ElementType = s.DataSource.ElementType
+		}
+	}
+
+	a.Validators = append(a.Validators, common.Validators...)
+	a.Validators = append(a.Validators, dataSource.Validators...)
+
+	if s.Common != nil {
+		if s.Common.CustomType != nil {
+			a.CustomType = s.Common.CustomType
+		}
+	}
+
+	if s.DataSource != nil {
+		if s.DataSource.CustomType != nil {
+			a.CustomType = s.DataSource.CustomType
+		}
+	}
+	// * If user has not provided a custom type, we will use the default supertypes
+	if a.CustomType == nil {
+		a.CustomType = supertypes.NewListTypeOf[T](ctx)
+	}
+
+	deprecationMessage := ""
+	if s.Deprecated != nil {
+		a.DeprecationMessage = s.Deprecated.DeprecationMessage
+		deprecationMessage = s.Deprecated.computeDeprecatedDocumentation()
+	}
+
+	a.MarkdownDescription = genDataSourceAttrDescription(ctx, a.MarkdownDescription, deprecationMessage, a.Validators)
+	return a
+}
